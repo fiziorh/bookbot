e2ba658282bfa1e4f959c5919d59f1cef6b7af8f
fiziorh <81397105+fiziorh@users.noreply.github.com>
2023-09-04 17:48:52 +0700

    added 4 functions: main, get_chars_dict,get_chars_list,print_list. The project is all done git add .

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
+#Book path
 book_path = "books/frankenstein.txt"
 
+#Function
+def main():
+    print_list()
+
 def read_text():
-    text = get_book(book_path)
+    text = get_book()
     print(text)
 
 def count_words():
-    i = 0
-    text = get_book(book_path)
+    text = get_book()
     words = text.split()
-    for word in words:
-        i+=1
-    print(i)
+    return len(words)
 
-def get_book(path):
-    with open(path) as f:
+def get_book():
+    with open(book_path) as f:
         return f.read()
 
-read_text()
-count_words()
+def get_chars_dict():
+    chars = {}
+    text = get_book()
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_chars_list():
+    chars = get_chars_dict()
+    sorted_list = []
+    chars_list = list(chars.items())
+    for i in range(0,len(chars_list)-1):
+        if chars_list[i][0].isalpha()== True:
+            sorted_list.append(chars_list[i])
+    sorted_list = sorted(sorted_list, key=lambda x: x[1], reverse=True)
+    return sorted_list
+
+def print_list():
+    sorted_list = get_chars_list()
+    words = count_words()
+    print(f"--- Begin report of {book_path} ---\n {words} words found in the document\n")
+    for items in sorted_list:
+        print(f"The '{items[0]}' character was found {items[1]} times\n")
+    print("--- End report ---")
+    
+
+
+
+#Execute function
+main()
